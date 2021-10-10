6b147b63f64c86501095eaa7fe1d5cc68bd76cd5
TiltedDFA <p5mtremain@rmgs.org.uk>
2021-10-10 22:59:27 +0100

    a

--- GraphicsCW/Display.hpp
+++ GraphicsCW/Display.hpp
@@ -3,6 +3,19 @@
 #define DISPLAY_HPP
 #include "raylib.h"
 
+void LoadNeededFiles()
+{
+	Texture2D TitleScreen = LoadTexture("TitleScreen.png");
+	Texture2D  .Table = LoadTexture("TableTwo.png");
+	Texture2D CardBacking = LoadTexture("170CardBacking.png");
+	SetTargetFPS(60);
+}
+
+void DisplayMenu() 
+{
+
+}
+
 void DisplayBackOfCards(int X_Pos, int Y_Pos, int CardsToDisplay, Texture2D& Card) // (Xpos, Ypos, AmountOfCards, Cardtext)
 {
 	for (int i = 0; i < CardsToDisplay; i++, X_Pos += 125)
--- GraphicsCW/main.cpp
+++ GraphicsCW/main.cpp
@@ -31,31 +31,26 @@ int main(void)
 	const int screenWidth = 1920;
 	const int screenHeight = 1080;
 	InitWindow(screenWidth, screenHeight, "Durak");
-	Image _Table = LoadImage("TableTwo.png");
-	Image _CardBacking = LoadImage("170CardBacking.png");
-	Texture2D  Table = LoadTextureFromImage(_Table);
-	Texture2D CardBacking = LoadTextureFromImage(_CardBacking);
-	UnloadImage(_CardBacking);
-	UnloadImage(_Table);
-	SetTargetFPS(60);
+	LoadNeededFiles();
 	Deck deck;
 	//--------------------------------------------------------------------------------------
 	// Main game loop
 	while (!WindowShouldClose())    // Detect window close button or ESC key
 	{
+		//Doesn't work lol fix it (Issue is likely the LoadNeedeFiles())
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-		DrawTexture(Table, 0, 0, WHITE);
-		DisplayBackOfCards(600, 75, 6, CardBacking);
-		DisplayBackOfCards(600, 840, 6, CardBacking);
-		DrawText(TextFormat("Master suit is: %d", ((int)deck.getMasterSuit())), 200, 120, 20, WHITE);
+		DrawTexture(TitleScreen, 0, 0, WHITE);
+		//DisplayBackOfCards(600, 75, 6, CardBacking);
+		//DisplayBackOfCards(600, 840, 6, CardBacking);
+		//DrawText(TextFormat("Master suit is: %d", ((int)deck.getMasterSuit())), 200, 120, 20, WHITE);
 		EndDrawing();
 		//----------------------------------------------------------------------------------
 	}
 
 	// De-Initialization
 	//--------------------------------------------------------------------------------------
-	UnloadTexture(Table);
+	UnloadTexture(.Table);
 	UnloadTexture(CardBacking);
 	CloseWindow();        // Close window and OpenGL context
 	//--------------------------------------------------------------------------------------
